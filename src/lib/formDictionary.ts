257a523ddbb0747aecd52684f4145496db6ab8d7
Guilherme Tonello <a57064@alunos.ipb.pt>
2024-04-17 03:22:50 +0100

    refact: Change products names

--- src/lib/formDictionary.ts
+++ src/lib/formDictionary.ts
@@ -1,28 +1,28 @@
 
 export const skinType = {
-  "1": { en: "Dry", pt: "Seco" },
+  "1": { en: "Dry", pt: "Seca" },
   "2": { en: "Sensitive", pt: "Sensível" },
-  "3": { en: "Sensible", pt: "Sensato" },
-  "4": { en: "Extra Dry", pt: "Extra Seco" },
-  "5": { en: "Combined", pt: "Combinado" },
-  "6": { en: "Irritated", pt: "Irritado" },
+  "3": { en: "Sensible", pt: "Sensibilizada" },
+  "4": { en: "Extra Dry", pt: "Muito Seca" },
+  "5": { en: "Combined", pt: "Mista" },
+  "6": { en: "Irritated", pt: "Irritada" },
   "7": { en: "Normal", pt: "Normal" },
   "8": { en: "Atopic Tendency", pt: "Tendência Atópica" },
-  "9": { en: "Fragile Damaged", pt: "Danificado Frágil" },
+  "9": { en: "Fragile Damaged", pt: "Danificada e Frágil" },
   "0": { en: "Not Sure", pt: "Não Tenho Certeza" },
 };
 
 export const skinDisease = {
-  "1": { en: "Translucency", pt: "Translucidez" },
-  "2": { en: "Eye Area", pt: "Área dos Olhos" },
-  "3": { en: "Uniformness", pt: "Uniformidade" },
+  "1": { en: "Translucency", pt: "Pele Opaca" },
+  "2": { en: "Eye Area", pt: "Olheira" },
+  "3": { en: "Uniformness", pt: "Tom Desigual" },
   "4": { en: "Redness", pt: "Vermelhidão" },
   "5": { en: "Sagging", pt: "Flacidez" },
-  "6": { en: "Wrinkles", pt: "Rugas" },
+  "6": { en: "Wrinkles", pt: "Linhas de expressão" },
   "7": { en: "Acne", pt: "Acne" },
-  "8": { en: "Hydration", pt: "Hidratação" },
-  "9": { en: "Pigmentation", pt: "Pigmentação" },
-  "10": { en: "Blackhead", pt: "Cravo" },
+  "8": { en: "Hydration", pt: "Desidratação" },
+  "9": { en: "Pigmentation", pt: "Baixa pigmentação"},
+  "10": { en: "Blackhead", pt: "Cravos" },
   "11": { en: "Pores", pt: "Poros" },
 };
 
